026924e52da70f38db293b1100bf8a893be9c741
effattoma <effat35-2146@diu.edu.bd>
2021-09-21 21:47:01 +0600

    add Counter

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// count
+
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+       range = end - start,
+         increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+
+            }, step);
+    }
+
+    counter("count1", 0, 1287, 3000);
+    counter("count2", 10, 1287, 3000);
+    counter("count3", 2000, 1287, 3000);
+});
